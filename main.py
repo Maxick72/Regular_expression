261069ff8e29d8cb94d75baf16a0a811e218fa7a
Mikhail Petrov <mpetrov71@mail.ru>
2026-04-22 00:12:19 +0500

    corrected main.py row 17

--- main.py
+++ main.py
@@ -14,14 +14,18 @@ for row in contacts_list[1:]:
         key = (fio_parts[0], fio_parts[1], fio_parts[2])
     else:
         key = (fio_parts[0], fio_parts[1])
-    if key not in contacts_dict and not any(key in exist_key for exist_key in contacts_dict):
+
+    if not any(all(item in exist_key for item in key) for exist_key in contacts_dict):
         for i in range(3):
             row[i] = fio_parts[i] if i < len(fio_parts) else ""
         contacts_dict[key] = row
     else:
-        for i in range(3, len(row)):
-            if not contacts_dict[key][i]:
-                contacts_dict[key][i] = row[i]
+        for exist_key in contacts_dict:
+            for i in range(len(exist_key) - len(key) + 1):
+                if  exist_key[i:i + len(key)] == key:
+                    for i in range(3, len(row)):
+                        if not contacts_dict[exist_key][i]:
+                            contacts_dict[exist_key][i] = row[i]
 #pprint(contacts_dict.values())
 updated_contacts_list = [contacts_list[0]] + list(contacts_dict.values())
 phone_pattern = r"(\+7|8)?\s*\(?(\d{3})\)?[\s-]*(\d{3})[\s-]*(\d{2})[\s-]*(\d{2})"
@@ -32,7 +36,6 @@ for row in updated_contacts_list[1:]:
     row[5] = re.sub(phone_pattern,repl_phone_pattern,row[5])
     row[5] = re.sub(ext_pattern, repl_ext_pattern, row[5])
 #pprint(updated_contacts_list)
-
 # TODO 2: сохраните получившиеся данные в другой файл
 # код для записи файла в формате CSV
 with open("phonebook.csv", "w", encoding="utf-8", newline = "") as f:

bd060fa3a699e0ca9e7f671034838675a5003bca
Mikhail Petrov <mpetrov71@mail.ru>
2026-04-21 10:34:41 +0500

    Init

---
+++ main.py
@@ -0,0 +1,34 @@
+from pprint import pprint
+# читаем адресную книгу в формате CSV в список contacts_list
+import csv
+import re
+with open("phonebook_raw.csv", encoding="utf-8") as f:
+  rows = csv.reader(f, delimiter=",")
+  contacts_list = list(rows)
+#pprint(contacts_list)
+contacts_dict = {}
+# TODO 1: выполните пункты 1-3 ДЗ
+for row in contacts_list[1:]:
+    fio_parts = " ".join (row [:3]).split()
+    key = (fio_parts[0], fio_parts[1])
+    if key not in contacts_dict:
+        for i in range(3):
+            row[i] = fio_parts[i] if i < len(fio_parts) else ""
+        contacts_dict[key] = row
+    else:
+        for i in range(3, len(row)):
+            if not contacts_dict[key][i]:
+                contacts_dict[key][i] = row[i]
+updated_contacts_list = [contacts_list[0]] + list(contacts_dict.values())
+phone_pattern = r"(\+7|8)?\s*?\(?(\d{3,5})\)?\s*?(\d{1,3})[-\s]?(\d{2})[-\s]?(\d{2}?)"
+replacement_pattern = r"+7(\2)\3-\4-\5"
+for row in updated_contacts_list[1:]:
+    row[5] = re.sub(phone_pattern,replacement_pattern,row[5])
+#pprint(updated_contacts_list)
+
+# TODO 2: сохраните получившиеся данные в другой файл
+# код для записи файла в формате CSV
+with open("phonebook.csv", "w", encoding="utf-8") as f:
+  datawriter = csv.writer(f, delimiter=',')
+  # Вместо contacts_list подставьте свой список
+  datawriter.writerows(updated_contacts_list)

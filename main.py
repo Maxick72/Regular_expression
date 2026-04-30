0bb5dad19af2bbe3b936b8cf9cb087d290ffcd1f
Mikhail Petrov <mpetrov71@mail.ru>
2026-04-30 13:53:13 +0500

    corrected main.py for code optimisation

--- main.py
+++ main.py
@@ -10,20 +10,41 @@ contacts_dict = {}
 # TODO 1: выполните пункты 1-3 ДЗ
 for row in contacts_list[1:]:
     fio_parts = " ".join (row [:3]).split()
+    # print(fio_parts)
     if len(fio_parts) >= 3:
         key = (fio_parts[0], fio_parts[1], fio_parts[2])
-    else:
+    elif len(fio_parts) >= 2:
         key = (fio_parts[0], fio_parts[1])
+    else:
+        print (f' Отсутствует ФАМИЛИЯ или ИМЯ сотрудника {fio_parts} , запись не обработана')
+        continue
 
     if not any(all(item in exist_key for item in key[:len(exist_key)]) for exist_key in contacts_dict):
         for i in range(3):
             row[i] = fio_parts[i] if i < len(fio_parts) else ""
         contacts_dict[key] = row
+
     else:
         for exist_key in contacts_dict:
-            for i in range(2, len(row)):
-                if not contacts_dict[exist_key][i]:
-                    contacts_dict[exist_key][i] = row[i]
+            if len(exist_key) >= 3 :
+                # for i in range(len(exist_key) - len(key) + 1):
+                if exist_key[0:3] == key or exist_key[0:2] == key :
+                    for i in range(3, len(row)):
+                        if not contacts_dict[exist_key][i]:
+                            contacts_dict[exist_key][i] = row[i]
+                        # else:
+                        #     for i in range(3):
+                        #         row[i] = fio_parts[i] if i < len(fio_parts) else ""
+                        #     contacts_dict[key] = row
+            elif len(exist_key) == 2:
+                # for i in range(len(exist_key) - len(key) + 1):
+                if exist_key == key or exist_key == key[0:2]:
+                    for i in range(3, len(row)):
+                        if not contacts_dict[exist_key][i]:
+                            contacts_dict[exist_key][i] = row[i]
+            else:
+                print(f' Отсутствует ФАМИЛИЯ или ИМЯ сотрудника {key} , запись не обработана')
+
 #pprint(contacts_dict.values())
 updated_contacts_list = [contacts_list[0]] + list(contacts_dict.values())
 phone_pattern = r"(\+7|8)?\s*\(?(\d{3})\)?[\s-]*(\d{3})[\s-]*(\d{2})[\s-]*(\d{2})"
